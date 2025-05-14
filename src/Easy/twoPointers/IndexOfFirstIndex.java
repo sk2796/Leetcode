f5ac1dcfa662a3b5944e0bcd2fdf0cfba353b57e
Shubham Kumar <shubhamskkeshri@gmail.com>
2025-05-15 01:28:53 +0530

    leetcode

--- src/Easy/twoPointers/IndexOfFirstIndex.java
+++ src/Easy/twoPointers/IndexOfFirstIndex.java
@@ -3,16 +3,23 @@ package Easy.twoPointers;
 public class IndexOfFirstIndex {
     public static void main(String[] args) {
         String haystack = "fdsadjkasad", needle = "sad";
-        int index = strStr(haystack, needle);
-        System.out.println(index);
+//        int index = strStr(haystack, needle);
+//        System.out.println(index);
 
-        String sentence = "i love eating burger", searchWord = "burg";
+        String sentence = "hellohello hellohellohello", searchWord = "ell";
         int index1 = searchWordInSentence(sentence, searchWord);
         System.out.println(index1);
     }
 
     private static int searchWordInSentence(String sentence, String searchWord) {
         int index = -1;
+        String[] s = sentence.split(" ");
+        for (int i = 0; i < s.length; i++) {
+            if(s[i].contains(searchWord) && s[i].startsWith(searchWord)) {
+                index = i+1;
+                return index;
+            }
+        }
         
         return index;
     }
