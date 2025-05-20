f1a959c0e461accf8d1278eaa90caeb8e54995f7
Shubham Kumar <shubhamskkeshri@gmail.com>
2025-05-20 23:35:41 +0530

    leetcode

---
+++ src/Easy/Java8.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Java8 {
+    public static void main(String[] args) {
+        int[] arr = {5,8,7,6,9,2,3,5,4,2,6,8,9,7,7,4,6,6,9,8,4};
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for(int x : arr) {
+            if(hmap.containsKey(x)) {
+                hmap.put(x, hmap.get(x)+1);
+            } else hmap.put(x, 1);
+        }
+        System.out.println(hmap);
+        int max = hmap.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+        int min = hmap.entrySet().stream().min(Map.Entry.comparingByValue()).get().getKey();
+        System.out.println(max +"  "+min);
+    }
+}
