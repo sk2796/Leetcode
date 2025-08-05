13506b67f4ad722b410a0385b4735337a8cce674
Shubham Kumar <shubhamskkeshri@gmail.com>
2025-08-05 23:12:49 +0530

    leetcode

--- src/hackerRank/FindDuplicateUsingBitsManipulation.java
+++ src/hackerRank/FindDuplicateUsingBitsManipulation.java
@@ -2,10 +2,18 @@ package hackerRank;
 
 public class FindDuplicateUsingBitsManipulation {
     public static void main(String[] args) {
-        String str = "shubham";
+        String str = "shubhamskkei";
         printDuplicates(str);
     }
 
     private static void printDuplicates(String str) {
+        int h = 0, x = 0;
+        for (char c : str.toCharArray()) {
+            x = 1;
+            x = x<< (c - 97);
+            if((x & h) > 0) {
+                System.out.println("dup :"+ c);
+            } else h = x | h;
+        }
     }
 }
